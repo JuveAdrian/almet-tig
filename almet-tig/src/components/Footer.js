f11e88707daa0777afdb7ce4ecc823f34798081e
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-08-22 03:15:35 +0200

    save

--- almet-tig/src/components/Footer.js
+++ almet-tig/src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { Grid, Typography, Divider } from '@material-ui/core';
+import { Grid, Typography, Divider, TextField } from '@material-ui/core';
 
 class Footer extends React.Component {
     render () {
         return (
             <Grid container xs={12} style={{ backgroundColor: '#1769aa', color: '#fff', display: 'flex', alignItems: 'flex-end', bottom: '0' }}>
 
-                <Grid item container xs={11} style={{ borderRadius: '20px', border: '1px solid #ddd', backgroundColor: '#1769aa', marginTop: '20px' }}>
+                <Grid item container xs={11} style={{ borderRadius: '20px', border: '1px solid #ddd', backgroundColor: '#1769aa', margin: '20px 0' }}>
                     <Grid item xs={6} style={{ padding: '30px 20px', textAlign: 'center'}}>
                         <Typography variant="h6" style={{ color: '#ddd' }}>
                             ZADZWOŃ DO NAS!
@@ -29,7 +29,12 @@ class Footer extends React.Component {
                         </Typography>
                     </Grid>
                     <Grid item xs={6}>
-
+                        <form noValidate autoComplete="off" style={{ color: '#fff', padding: '30px 70px' }}>
+                            <TextField id="outlined-basic" label="Imię i Nazwisko" variant="outlined" style={{ backgroundColor: '#104773', float: 'left', border: 'none', color: '#fff', marginBottom: '10px' }} />
+                            <TextField id="outlined-basic" label="Email" variant="outlined" style={{ backgroundColor: '#104773', float: 'left', border: 'none', color: '#fff', marginBottom: '10px' }} />
+                            <TextField id="outlined-basic" label="Telefon" variant="outlined" style={{ backgroundColor: '#104773', float: 'left', border: 'none', color: '#fff', marginBottom: '10px' }} />
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" style={{ backgroundColor: '#104773', float: 'left', border: 'none', color: '#fff', marginBottom: '10px' }} />
+                        </form>
                     </Grid>
                 </Grid>
 
